ab75b175b859f741710d649d800b5105d6b82f64
Carlos lorenzo <clorenzozuniga@gmail.com>
2024-12-01 20:22:35 +0100

    query infers data processor dimension

--- forexgym/utils/query.py
+++ forexgym/utils/query.py
@@ -1,8 +1,11 @@
 from typing import List, Dict
 from collections.abc import Callable
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from dataclasses import dataclass, field
+
+import numpy as np
+import pandas as pd
 
 from .timeframe import Timeframe, available_timeframes
 
@@ -11,7 +14,9 @@ class Query:
     episode_length: int
     trading_timeframe: str
     trading_column: str
-    queries: List[Dict[str, Timeframe | int | Callable ]] = field(init=False, default_factory=list)
+    lookback: int = field(default=100)
+    queries: List[Dict[str, Timeframe | int | Callable]] = field(init=False, default_factory=list)
+    
     
     def add_query(self, timeframe: Timeframe, window_size: int, data_processor: Callable = None) -> None:
         if timeframe not in available_timeframes:
@@ -31,6 +36,23 @@ class Query:
     
     @property
     def observation_size(self) -> int:
-        return sum([query["window_size"] * 5 for query in self.queries]) # The 5 is a temporal hack, it will be inferred from data_processor function
+        # Create sample df
+        ohlc = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close"])
+        start = datetime(2000, 1, 1, 1)
+        end = start + timedelta(hours=self.lookback-1)
+        ohlc["Date"] = pd.date_range(start=start, end=end, freq=timedelta(hours=1))
+        ohlc["Open"] = list(range(0, self.lookback))
+        ohlc["High"] = list(range(0, self.lookback))
+        ohlc["Low"] = list(range(0, self.lookback))
+        ohlc["Close"] = list(range(0, self.lookback))
+        ohlc["Volume"] = list(range(0, self.lookback))
+        
+        
+        
+        return sum([query["window_size"] * self._data_processor_dimension(ohlc, query) for query in self.queries])
     
-    
+    def _data_processor_dimension(self, df: pd.DataFrame, query: Dict[str, Timeframe | int | Callable]) -> int:
+        try:
+            return query["data_processor"](df).shape[1]
+        except ValueError:
+            raise ValueError(f"Data processor looks way into the past to infer output. Set lookback on init to requiered depth")
